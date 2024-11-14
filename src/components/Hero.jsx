d8a7d202cac720b4138fa932e69b03ff12e51eaa
Tanmay Tiwari <tanmaytiwari200905@gmail.com>
2024-11-14 23:01:43 +0530

    Responsiveness Added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <motion.img
           src="left.png"
           alt=""
-          className="absolute top-[50%] h-[115vh] object-contain -left-44 "
+          className="absolute top-[50%] h-[115vh] object-contain -left-44 hidden lg:block"
           initial={{ x: "-100%", opacity: 0 }} // Start from off-screen to the left
           animate={{ x: 0, opacity: 1, rotate: -6 }} // Move to the final position
           transition={{ ease: [0.39, 0.24, 0.3, 1], duration: 2 }}
@@ -15,7 +15,7 @@ const Hero = () => {
         <motion.img
           src="right.png"
           alt=""
-          className="absolute h-[88vh] -right-56 -top-[5%]"
+          className="absolute h-[88vh] -right-56 -top-[5%] hidden lg:block"
           initial={{ x: "100%", opacity: 0 }} // Start from off-screen to the right
           animate={{ x: 0, opacity: 1, rotate: 12 }} // Move to the final position
           transition={{ ease: [0.39, 0.24, 0.3, 1], duration: 2 }}
@@ -23,22 +23,22 @@ const Hero = () => {
         <img
           src="HackShastra.png"
           alt=""
-          className="absolute -z-1 top-[10%] w-[80vw] h-[80vh] opacity-15 object-cover"
+          className="absolute -z-1 top-[10%] lg:w-[80vw] opacity-15 object-cover"
         />
         <div className="flex items-center justify-center flex-col mt-24 z-10">
-          <h1 className="text-9xl font-extrabold text-white">HackShastra</h1>
-          <h2 className="text-[3.85vh] tracking-wide font-bold text-white">
+          <h1 className="text-[6vh] md:text-7xl lg:text-[18vh] font-extrabold text-white text-center">HackShastra</h1>
+          <h2 className="text-xs md:text-2xl lg:text-[4vh] tracking-wide font-bold text-white text-center md:mt-4">
             INNOVATION WITH ANCIENT MYTHOLOGICAL SPIRIT
           </h2>
         </div>
       </div>
       <motion.div
-        className="absolute right-0 top-[80%] bg-[#181818] px-8 py-5 rounded-l-3xl"
+        className="absolute right-0 bottom-8 md:bottom-16 lg:bottom-24 bg-[#181818] px-4 py-3 md:px-6 md:py-4 lg:px-8 lg:py-5 rounded-l-3xl max-w-[90%] sm:max-w-[80%] md:max-w-[60%] lg:max-w-[40%]"
         initial={{ x: "100%", opacity: 0 }} // Initially off-screen (to the right)
         animate={{ x: 0, opacity: 1 }} // Slide in to the final position and become fully visible
         transition={{ ease: [0.39, 0.24, 0.3, 1], duration: 2 }} // Smooth transition
       >
-        <p className="text-2xl font-semibold">
+        <p className="lg:text-2xl md:text-xl sm:text-base font-semibold">
           Join, create, and{" "}
           <span className="text-[#E50914] font-bold">Conquer</span> real-world{" "}
           <br /> <span className="text-[#E50914] font-bold">Challenges</span>{" "}
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,22 +3,27 @@ import { useState } from "react";
 
 const Nav = () => {
   const [isHovered, setIsHovered] = useState(false);
+  const [isOpen, setIsOpen] = useState(false); // State to toggle mobile menu modal
+
   return (
-    <nav className="flex justify-between px-10 text-xl">
-      <div className="px-8">
+    <nav className="flex justify-between px-6 md:px-10 text-xl relative">
+      {/* Left Section */}
+      <div className="px-[2vw] md:px-[4vw]">
         <motion.div
-          className="bg-[#181818] w-[15vw] h-[25vh] rounded-b-3xl p-4 relative"
+          className="bg-[#181818] w-[51.5vw] md:w-[26.5vw] lg:w-[15.5vw] h-[19vh] md:h-[20vh] lg:h-[25vh] rounded-b-3xl p-4 relative"
           initial={{ y: -200, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ ease: [0.39, 0.24, 0.3, 1], duration: 2 }}
         >
-          <h1 className="bg-[#181818] font-black text-2xl mt-3">
+          <h1 className="bg-[#181818] font-black text-lg md:text-xl lg:text-2xl mt-2 md:mt-3">
             2025 Jan. - Feb.
           </h1>
-          <p className="bg-[#181818] font-extrabold mt-3">Offline</p>
+          <p className="bg-[#181818] font-extrabold text-sm md:text-base lg:text-xl mt-2 md:mt-3">
+            Offline
+          </p>
 
           <motion.button
-            className="flex items-center justify-between bg-white w-[13vw] h-[5vh] rounded-lg mt-5 absolute z-10"
+            className="flex items-center justify-center gap-2 lg:gap-3 md:gap-4 bg-white lg:w-[13vw] md:w-[22vw] h-[5vh] md:h-[5vh] rounded-lg mt-2 md:mt-2 lg:mt-5 absolute z-10"
             onHoverStart={() => setIsHovered(true)}
             onHoverEnd={() => setIsHovered(false)}
             whileHover={{
@@ -27,16 +32,15 @@ const Nav = () => {
             }}
             transition={{ duration: 0.3 }}
           >
-            {/* Show either the image or the SVG based on hover state */}
             {!isHovered ? (
               <motion.img
                 src="devfolio.png"
                 alt="devfolio"
-                className="ml-2 w-[3vh] h-[3vh]"
+                className="ml-2 w-[2.5vh] lg:w-[3vh] md:w-[2.5vh] "
               />
             ) : (
               <svg
-                className="w-[3vh] h-[3vh] ml-2"
+                className="ml-2 w-[2.5vh] lg:w-[3vh] md:w-[2.5vh]"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="#fff"
                 viewBox="0 0 115.46 123.46"
@@ -45,10 +49,8 @@ const Nav = () => {
                 <path d="M110.93 55.87A55.43 55.43 0 0 1 60.08 111s-36.48.92-48.58-.12C5 110.29.15 104.22 0 97.52l.2-83.84C.38 7 5.26.94 11.76.41c12.11-1 48.59.12 48.59.12a55.41 55.41 0 0 1 50.58 55.34z"></path>
               </svg>
             )}
-
-            {/* Text stays the same but aligns to center smoothly on hover */}
             <motion.p
-              className="text-sm text-black font-bold mr-4"
+              className="text-xs md:text-xs lg:text-sm text-black font-bold mr-4"
               animate={{
                 color: isHovered ? "white" : "black",
               }}
@@ -59,25 +61,70 @@ const Nav = () => {
           </motion.button>
         </motion.div>
       </div>
-      <div className="flex gap-14 font-semibold py-8 mr-8 absolute right-8 z-10">
+
+      {/* Hamburger Icon for Mobile */}
+      <div className="md:hidden flex items-center">
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="text-white focus:outline-none"
+        >
+          <svg
+            className="w-8 h-8"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}
+            />
+          </svg>
+        </button>
+      </div>
+
+      {/* Links for larger screens */}
+      <div className="hidden md:flex gap-10 md:gap-5 lg:gap-14 font-semibold py-8 md:mr-4 lg:mr-8 absolute z-10 right-4">
         {["Timeline", "Mentors", "Our Team", "Sponsors", "Contact Us"].map(
           (item, index) => (
             <motion.a
               key={index}
-              href=""
-              className="relative group" // Add group class to manage hover interactions
-              whileHover={{ color: "#f0f0f0" }} // Change text color on hover
+              href="#"
+              className="relative group text-white md:text-base lg:text-xl"
+              whileHover={{ color: "#f0f0f0" }}
             >
               {item}
-              {/* Underline Animation */}
-              <motion.span
-                className="absolute top-7 left-0 w-full h-[2px] bg-white origin-left scale-x-0 group-hover:scale-x-100 transition-all duration-300"
-                // group-hover scales the underline when the parent .group is hovered
-              />
+              <motion.span className="absolute top-7 left-0 w-full h-[2px] bg-white origin-left scale-x-0 group-hover:scale-x-100 transition-all duration-300" />
             </motion.a>
           )
         )}
       </div>
+
+      {/* Mobile Menu Modal */}
+      {isOpen && (
+        <div className="fixed inset-0 bg-[#181818] bg-opacity-90 z-20 flex flex-col items-center justify-center space-y-8 text-white text-2xl font-semibold">
+          {["Timeline", "Mentors", "Our Team", "Sponsors", "Contact Us"].map(
+            (item, index) => (
+              <a
+                key={index}
+                href="#"
+                className="hover:text-gray-300 text-center"
+                onClick={() => setIsOpen(false)} // Close modal on link click
+              >
+                {item}
+              </a>
+            )
+          )}
+          <button
+            className="text-white absolute top-6 right-6 text-3xl focus:outline-none"
+            onClick={() => setIsOpen(false)}
+          >
+            &times;
+          </button>
+        </div>
+      )}
     </nav>
   );
 };
